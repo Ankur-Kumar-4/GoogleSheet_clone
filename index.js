33c0298f1a1e4be37b493d3aca505adc32b2c66e
Ankurdyno <98178905+Ankurdyno@users.noreply.github.com>
2023-10-07 12:50:17 +0530

    v-2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const header = document.getElementById("header");
 const body = document.getElementById("body");
-
+const sheet_container =  document.getElementById("add_sheet_container");
 
 
 for (let i=65;i<=90;i++){
@@ -45,6 +45,18 @@ for (let i = 1; i <= 100; i++) {
   sl_container.appendChild(sl);
 }
 
+let i = 1;
+document.getElementById("addSheet_btn").addEventListener("click",()=>{
+    let sheet = document.createElement("div");
+    sheet.innerHTML=`<div class="sheet" id="${i}" onclick="active_clss(this)">
+         <span>Sheet ${i}</span>
+         </div>
+    `
+    sheet_container.appendChild(sheet);
+    i++;
+});
 
-
+function active_clss(sheetBtn) {
+    sheetBtn.classList.toggle("active-option");
+}
 
--- options.js
+++ options.js
@@ -1,9 +1,174 @@
+const cellName = document.getElementById("cell-name");
+const textAlignElement = document.getElementsByClassName("text_align");
+const boldBtn = document.getElementById("bold");
 let activeCell = null;
 
-const cellName = document.getElementById("cell-name");
+const default_optionState = {
+    fontFamily:"",
+    isBoldSelected: false,
+    isItalicSelected:false,
+    isUnderlineSelected:false,
+    textAlign:'left',  // it can have "left"| "right"|"center"
+    textColor:"#000",
+    backgroundColor: "#fff",
+    fontSize:14,
+};
+
+const activeOptionState = {};
+
+function highlightOptionsButton() {
+    
+if(activeOptionState.isBoldSelecte && !boldBtn.classList.contains("active-option")){
+    boldBtn.classList.add("active-option");
+}else{
+    boldBtn.classList.remove("active-option");
+}
+
+
+}
+
 
 
 function OncellFocus(e){
-    activeCell = e.target.id;
-    cellName.innerText=activeCell;
+
+    if (activeCell && activeCell.id === e.target.id){
+        
+        return;
+
+    }
+    activeCell = e.target;
+    cellName.innerText=e.target.id;
+
+    const computedStyle = getComputedStyle(activeCell);
+
+    const activeOptionState = {
+    fontFamily: computedStyle.fontFamily,
+    isBoldSelected: computedStyle.fontWeight ==="600",
+    isItalicSelected: computedStyle.fontStyle === "italic",
+    isUnderlineSelected: computedStyle.fontStyle === "underline",
+    textAlign:computedStyle.textAlign,
+    textColor:computedStyle.color,
+    backgroundColor:computedStyle.backgroundColor,
+    fontSize:computedStyle.fontSize,
+    }
+    
+    highlightOptionsButton();
 }
+
+
+function onClickBold(boldButton){
+    // this func will be tiggered when user clicks on bold button.
+
+    boldButton.classList.toggle("active-option");
+    // const selectedCell = document.getElementById(activeCell);
+
+        if(activeCell){           
+
+            if(activeOptionState.isBoldSelected == false){
+                activeCell.style.fontWeight="600";
+              
+
+            }else{
+                activeCell.style.fontWeight="400"
+                
+            }
+            activeOptionState.isBoldSelected = !activeOptionState.isBoldSelected;
+        }
+   
+
+}
+
+
+
+function onClickItalic (italicButton){
+
+    italicButton.classList.toggle("active-option");
+
+    if(activeCell){
+        
+        if(activeOptionState.isItalicSelected){
+            activeCell.style.fontStyle = "normal";
+            activeOptionState.isItalicSelected = false;
+        }else{
+            activeCell.style.fontStyle = "italic";
+            activeOptionState.isItalicSelected = true;
+        }
+
+    }
+}
+
+
+function onClickUnderline(underlineButton) {
+    underlineButton.classList.toggle("active-option");
+
+    if(activeCell){
+
+        if (activeOptionState.isUnderlineSelected){
+            activeCell.style.textDecoration = "none";
+        }else{
+            activeCell.style.textDecoration = "underline";
+        }
+        activeOptionState.isUnderlineSelected = !activeOptionState.isUnderlineSelected;
+    }
+}
+
+function highlightTextAlignBtn(textAlignValue) {
+
+   
+
+    for (let i = 0 ; i < textAlignElement.length ; i++){
+
+        if(textAlignElement[i].getAttribute("data-value") == textAlignValue){
+
+            textAlignElement[i].classList.add("active-option");
+
+        } else {
+            textAlignElement[i].classList.remove("active-option");
+        
+        }
+    }
+}
+
+
+function onClickTextAlign (textAlignButton){
+     
+    let selectedVal = textAlignButton.getAttribute("data-value");
+    highlightTextAlignBtn(selectedVal);
+
+    if (activeCell){
+
+        activeCell.style.textAlign = selectedVal;
+        
+        activeOptionState.textAlign = selectedVal;
+
+    }
+}
+
+function changeTextColor(changeColor) {
+    
+    const selectedColor = changeColor.value;
+
+   if (activeCell){
+
+       activeCell.style.color= selectedColor;
+
+       activeOptionState.textColor = selectedColor;
+
+   }
+
+}
+
+function changeBgColor(bgColor) {
+    
+    const selectedColor = bgColor.value;
+
+   if (activeCell){
+
+       activeCell.style.backgroundColor= selectedColor;
+
+       activeOptionState.backgroundColor = selectedColor;
+
+   }
+
+}
+
